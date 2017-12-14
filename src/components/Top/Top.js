682728f2d879d3f65d14fb1077ae4203fea1ddb2
TheGreyHaven <haven.giguere@gmail.com>
2017-12-14 11:40:54 -0800

    added image

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
 import "./top.css";
+import womanGlassesblue from '../../images/womanGlasses350px.png';
+import womanGlassesyellow from '../../images/womanGlassesYellow350px.png';
 
 
-if( {props.dotColor} === "blue"){
-  let logoImg = "../../images/womanGlasses350px";
-}else{
-  let logoImg = "../../images/womanGlassesPink350px";
-}
 
 const Top = props => (
+
+  let currentImg;
+
+  if ( {props.dotColor} == "yellow"){
+    currentImg = {womanGlassesblue}
+  }else{
+    currentImg = {womanGlassesyellow}
+  }
+
   <div id="topFourth">
     <div id="dotDiv">
       <span className="circle" onClick={props.onClick} id={ props.dotColor }>
       </span>
     </div>
-    <span className="LogoCircle"><img src="logoImg"></img>
+    <span className="LogoCircle"><img src={currentImg}></img>
     </span>
-    <h1 align="right">Haven Giguere</h1>
+    <h1 align="right" className={ props.dotColor }>Haven Giguere</h1>
   </div>
 );
 
 export default Top;
-
-variable = (condition) ? true-value : false-value;

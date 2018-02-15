f51b241f13760b6a7e7a8dcefefe042d32d52ea8
TheGreyHaven <haven.giguere@gmail.com>
2018-02-15 10:48:08 -0800

    added more files for working link commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Preview from "./pages/Preview";
+import Exporter from "./pages/Exporter";
 
 //this is rendering index.js. Calling the Home folder it looks for index.js to render
 const App = () => (
   <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home" component={Home} />
-    </div>
+	    <div>
+	      <Route exact path="/" component={Home} />
+	      <Route exact path="/home" component={Home} />
+	      <Route exact path="/preview" component={Preview} />
+	      <Route exact path="/exporter" component={Exporter} />
+	    </div>
   </Router>
 );
 
